0c15ece272087b743339e5d6013fcf271dcb0477
Christina <Christinepeninsula@hotmail.com>
2019-02-19 21:29:17 -0500

    read up on react and used simplest method i could understand, am going to rebuild using the multiple class method we touched on today as well

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
+// MUST ALWAYS EXTEND FROM COMPONENT
+class App extends Component {
+    // ALWAYS HAS TO HAVE PROPS WHICH IS SHORT FOR PROPERTIES
+    constructor(props) {
+      // GETS THE PROPERTIES THAT ARE IN COMPONENT WITH SUPER
+    	super(props);
+    	// SETS THE STATE AS CLICKS = 0 TO BEGIN WITH
+    	this.state = {clicks: 0}
+    }
+
+    // METHOD LIKES
+    Likes() {
+      // TAKE THE CURRENT STATE OR NUMBER OF CLICKS, FAT ARROW ACTS AS ANONYMOUS FUNCTION
+      // THAT GIVES THE DIRECTIONS TO ADD 1 TO THE PREVIOUS STATE AND MAKE IT THE NEW NUMBER OF CLICKS
+    	this.setState((prevState) => ({
+      	   clicks: prevState.clicks + 1
+        }));
+    }
+
+    // RENDER METHOD
+    render() {
+      // RETURN IS REQUIRED IN EVERY CLASS AND LETS US INJECT HTML STYLE CODE INTO OUR JSX
+    	return (
+    	    // CREATED A DIV TAG TO HOLD MY BUTTON
+    	    <div>
+              {/*event handler built into button, binds it, gets current state of # of clicks, adds "likes" in text*/}
+              <button onClick={this.Likes.bind(this)}>{this.state.clicks} Likes </button>
+            </div>
+        )
+
+    }
+
+}
+//must export App everytime
 export default App;
